6dedbea57c56d9039584a9cea70dab6cdf088a83
Lorenzo Caldara <lorenzo@Lorenzos-MacBook-Air.local>
2016-03-21 14:56:11 +0100

    Creata versione non funzionante per leggere FDT CRM

---
+++ fdtCrm_middleman/lib/middleman_extension.rb
@@ -0,0 +1,1 @@
+require 'fdtCrm_middleman'
---
+++ fdtCrm_middleman/lib/syncEventi.rb
@@ -0,0 +1,104 @@
+require 'middleman-core/cli'
+require 'middleman-core/extensions'
+require 'date'
+require 'middleman-blog/uri_templates'
+require 'padrino-helpers'
+require 'padrino/rendering'
+
+
+# CLI Module
+module Middleman::Cli
+  # A thor task for creating new projects
+    class SyncEventi < Thor
+        include Thor::Actions
+        include ::Middleman::Blog::UriTemplates
+        include Padrino::Helpers
+
+
+        check_unknown_options!
+
+        namespace :syncEventi
+
+        def self.source_root
+            ENV['MM_ROOT']
+        end
+
+        desc 'syncEventi NAME [options]', 'Sincronizza gli eventi'
+
+        class_option :document_type,
+                    type: :string,
+                    aliases: '-T',
+                    desc: 'Il document type'
+
+        # The syncPages task
+        # @param [String] tag
+        def syncEventi()
+
+            prismic_middleman = shared_instance.prismic_middleman
+
+            shared_instance.logger.info "  Prismic Sync: Start..."
+
+            site = prismic_middleman.get_by_tags ('site')
+            siteData = prismic_middleman.getBlockData (site.first)
+
+            puts tag
+
+
+
+            prismic_middleman.get_by_tags(tag).each do |document|
+
+                pageData = prismic_middleman.getBlockData (document)
+                category = prismic_middleman.get_document(document["block.category"].id);
+
+                if category.slug == 'home'
+                    categorySlug = '';
+                else
+                    categorySlug = category.slug
+                end
+
+                @title = pageData ['title']
+                @slug  = pageData ['slug']
+                @categorySlug = categorySlug
+                @category = category
+                @pageData = pageData
+                @siteData = siteData
+
+                prismic_inst = shared_instance.prismic_middleman(options[:prismic])
+
+                path_template = uri_template prismic_inst.options.permalink
+                params = {category: @categorySlug, title: getUriTitle(pageData)}
+                article_path = apply_uri_template path_template, params
+
+                pageTemplate = getTemplateName (pageData ['pageType'])
+                template pageTemplate, File.join(shared_instance.source_dir, article_path + prismic_inst.options.default_extension)
+                #print template pageTemplate
+
+            end
+
+        end
+
+        private
+            def shared_instance
+                @shared_instance ||= ::Middleman::Application.server.inst
+            end
+
+            def getTemplateName (tamplateName='default')
+                'fdt_templates/' + tamplateName.downcase + '.erb'
+            end
+
+            def getUriTitle (pageData)
+                if pageData['urlType'] == 'Slug'
+                    pageData['slug']
+                else
+                    'index'
+                end
+            end
+    end
+
+    Base.register(Middleman::Cli::SyncEventi, 'syncEventi', 'syncEventi [options]', 'Synchronize eventi da FdT Crm')
+    Base.map('syncEventi' => 'syncEventi')
+
+    def self.exit_on_failure?
+        true
+    end
+end
